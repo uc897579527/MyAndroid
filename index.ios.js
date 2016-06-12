784798cfa042ea61aebcce814ae217e464457c14
winfore <simonu@163.com>
2016-06-12 16:24:16 +0800

    wangfan

--- index.ios.js
+++ index.ios.js
@@ -17,15 +17,7 @@ class MyProject extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+          Hello World!
       </View>
     );
   }
